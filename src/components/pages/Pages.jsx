45d6f54c9bd8fd354af8e8d28ad13fe167472d53
Mann Kawatkar <mannkawatkar1603@gmail.com>
2024-01-09 22:10:10 +0530

    2nd commit

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home/Home";
-import About from "./About";
-import Contact from "./Contact";
-import Privacy from "./Privacy";
+
 
 const Pages = () => {
   return (
     <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/contact" element={<Contact />} />
-    <Route path="/privacy" element={<Privacy />} />
   </Routes>
 
   );
